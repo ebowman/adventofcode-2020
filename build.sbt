84e9ebfbcdb8fd13291b08b3f817abb98b1cd7bb
Eric Bowman <ebowman@boboco.ie>
2020-12-02 09:25:30 +0100

    day 1

---
+++ build.sbt
@@ -0,0 +1,3 @@
+
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.2"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.2" % "test"
---
+++ src/main/scala/aoc01/Sum2020.scala
@@ -0,0 +1,12 @@
+package aoc01
+
+import scala.io.Source
+
+object Sum2020 extends App {
+  val inputs = Source.fromInputStream(Sum2020.getClass.getResourceAsStream("input.txt")).getLines().map(_.toInt).toSet
+
+  for (a <- inputs) {
+    val b = 2020 - a
+    if (inputs.contains(b)) println(a * b)
+  }
+}
