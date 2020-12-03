7b109bef9fc1ec5fd6b530229bcfe83926425c23
Eric Bowman <ebowman@boboco.ie>
2020-12-03 08:16:02 +0100

    day 3

--- src/main/scala/aoc03/Driver.scala
+++ src/main/scala/aoc03/Driver.scala
@@ -1,5 +1,26 @@
 package aoc03
 
 object Driver {
-  lazy val input = util.Loader(this, "input.txt")
+  lazy val input: Seq[String] = util.Loader(this, "input.txt").toIndexedSeq
+
+  case class Board(rows: IndexedSeq[String], ruleX: Int = 3, ruleY: Int = 1) {
+    val width: Int = rows(0).length
+  }
+
+  case class Cursor(board: Board, x: Int = 0, y: Int = 0) {
+    def next: Option[Cursor] = {
+      if (y + board.ruleY >= board.rows.length) None
+      else Some(Cursor(board, (x + board.ruleX) % board.width, y + board.ruleY))
+    }
+    def tree: Int = if (board.rows(y)(x) == '#') 1 else 0
+  }
+
+  @scala.annotation.tailrec
+  def recurse(cursor: Cursor, trees: Int = 0): Int = {
+    val t = trees + cursor.tree
+    val n = cursor.next
+    if (n.nonEmpty) recurse(n.get, t)
+    else t
+  }
+
 }
--- src/test/scala/aoc03/DriverSpec.scala
+++ src/test/scala/aoc03/DriverSpec.scala
@@ -4,7 +4,45 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should._
 
 class DriverSpec extends AnyFlatSpec with Matchers {
-  "Driver" should "do something" in {
-    println(Driver.input)
+  val testInput =
+    """
+      |..##.......
+      |#...#...#..
+      |.#....#..#.
+      |..#.#...#.#
+      |.#...##..#.
+      |..#.##.....
+      |.#.#.#....#
+      |.#........#
+      |#.##...#...
+      |#...##....#
+      |.#..#...#.#""".stripMargin.trim.split("\n").toIndexedSeq
+
+  val liveInput = util.Loader(this, "input.txt").toIndexedSeq
+
+  import Driver._
+  val testBoards = Seq(Board(testInput), Board(testInput, 1, 1), Board(testInput, 5, 1), Board(testInput, 7, 1), Board(testInput, 1, 2))
+  val liveBoards = Seq(Board(liveInput), Board(liveInput, 1, 1), Board(liveInput, 5, 1), Board(liveInput, 7, 1), Board(liveInput, 1, 2))
+
+  "Driver" should "do pass the given test case" in {
+    val board = Board(testInput)
+    recurse(Cursor(board)) shouldBe 7
+  }
+
+  it should "solve the puzzle (part 1)" in {
+    val board = Board(liveInput)
+    recurse(Cursor(board)) shouldBe 176
+  }
+
+  it should "solve the second puzzle test case" in {
+    testBoards.map { board => recurse(Cursor(board)) }.foldLeft(1) {
+      case (a: Int, b: Int) => a * b
+    } shouldBe 336
+  }
+
+  it should "solve the second puzzle live case" in {
+    liveBoards.map { board => recurse(Cursor(board)) }.foldLeft(1L) {
+      case (a: Long, b: Int) => a * b
+    } shouldBe 5872458240L
   }
 }
