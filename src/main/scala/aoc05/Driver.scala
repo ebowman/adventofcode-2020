8e37731ec6f62d4ca24a1dc320685e141d2d5e03
Eric Bowman <ebowman@boboco.ie>
2020-12-05 06:58:22 +0100

    2020/05 tidy-up

--- src/main/scala/aoc05/Driver.scala
+++ src/main/scala/aoc05/Driver.scala
@@ -5,11 +5,11 @@ import scala.annotation.tailrec
 trait Driver {
 
   case class SeatFinder(code: String, rowMin: Int = 0, rowMax: Int = 127, colMin: Int = 0, colMax: Int = 7) {
-    def next: Option[SeatFinder] = {
+    private def next: Option[SeatFinder] = {
       if (code.isEmpty) None
       else {
         code.head match {
-          case 'F' => Some(copy(code = code.tail, rowMax = rowMax - (rowMax - rowMin ) / 2 - 1))
+          case 'F' => Some(copy(code = code.tail, rowMax = rowMax - (rowMax - rowMin) / 2 - 1))
           case 'B' => Some(copy(code = code.tail, rowMin = rowMin + (rowMax - rowMin) / 2 + 1))
           case 'L' => Some(copy(code = code.tail, colMax = colMax - (colMax - colMin) / 2 - 1))
           case 'R' => Some(copy(code = code.tail, colMin = colMin + (colMax - colMin) / 2 + 1))
@@ -18,11 +18,11 @@ trait Driver {
     }
 
     def id = {
-      val (row, col) = solve
+      val (row, col) = seat
       row * 8 + col
     }
 
-    def solve: (Int, Int) = {
+    def seat: (Int, Int) = {
       @tailrec
       def recurse(f: SeatFinder): SeatFinder = {
         f.next match {
@@ -32,10 +32,7 @@ trait Driver {
       }
 
       val done = recurse(this)
-      assert(done.rowMin == done.rowMin, s"done = $done")
-      assert(done.colMin == done.colMax, s"done = $done")
       (done.rowMin, done.colMin)
     }
   }
-
 }
--- src/test/scala/aoc05/DriverSpec.scala
+++ src/test/scala/aoc05/DriverSpec.scala
@@ -6,13 +6,16 @@ import util.Loader
 
 class DriverSpec extends AnyFlatSpec with Matchers with Driver {
   "SeatFinder" should "solve known test cases" in {
-    SeatFinder("FBFBBFFRLR").solve shouldBe (44, 5)
-    SeatFinder("BFFFBBFRRR").solve shouldBe (70, 7)
-    SeatFinder("FFFBBBFRRR").solve shouldBe (14, 7)
-    SeatFinder("BBFFBBFRLL").solve shouldBe (102, 4)
+    SeatFinder("FBFBBFFRLR").seat shouldBe(44, 5)
     SeatFinder("FBFBBFFRLR").id shouldBe 357
+
+    SeatFinder("BFFFBBFRRR").seat shouldBe(70, 7)
     SeatFinder("BFFFBBFRRR").id shouldBe 567
+
+    SeatFinder("FFFBBBFRRR").seat shouldBe(14, 7)
     SeatFinder("FFFBBBFRRR").id shouldBe 119
+
+    SeatFinder("BBFFBBFRLL").seat shouldBe(102, 4)
     SeatFinder("BBFFBBFRLL").id shouldBe 820
   }
 
@@ -20,9 +23,7 @@ class DriverSpec extends AnyFlatSpec with Matchers with Driver {
     Loader(this, "input.txt").map(id => SeatFinder(id)).maxBy(_.id).id shouldBe 919
   }
   it should "solve the second part" in {
-    val tmp = Loader(this, "input.txt").map(id => SeatFinder(id)).map(sf => (sf.id, sf.code)).toMap
-    val id = (for (i <- 80 to 919 if !tmp.contains(i)) yield i).head
-    id shouldBe 642
+    val allTickets = Loader(this, "input.txt").map(id => SeatFinder(id)).map(sf => (sf.id, sf.code)).toMap
+    Range(allTickets.keys.min, allTickets.keys.max + 1).find(id => !allTickets.contains(id)) shouldBe Some(642)
   }
-
 }
