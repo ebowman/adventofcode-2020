823540d1ab82af8c956f65c4d0a7f7ecbf8e87f7
Eric Bowman <ebowman@boboco.ie>
2020-12-08 21:47:40 +0100

    turd polishing

--- src/main/scala/y2020/Day08.scala
+++ src/main/scala/y2020/Day08.scala
@@ -3,62 +3,7 @@ package y2020
 import scala.annotation.tailrec
 import scala.util.parsing.combinator.RegexParsers
 
-trait Day08 extends RegexParsers {
-
-  def nopsAndJumps(instructions: Seq[Instruction]): Seq[Int] = instructions.zipWithIndex.filter(x => x._1.isInstanceOf[Nop] || x._1.isInstanceOf[Jmp]).map(_._2)
-
-  case class ChangeMachine(instructions: IndexedSeq[Instruction], nopsAndJumps: Seq[Int], result: Int = -1) {
-    def hasNext: Boolean = result == -1
-
-    def flip(instructions: IndexedSeq[Instruction], c: Int): IndexedSeq[Instruction] = {
-      instructions(c) match {
-        case Nop(x) =>
-          (instructions.take(c) :+ Jmp(x)) ++ instructions.drop(c + 1)
-        case Jmp(x) =>
-          (instructions.take(c) :+ Nop(x)) ++ instructions.drop(c + 1)
-        case _ => sys.error(s"")
-      }
-    }
-
-    def next: ChangeMachine = {
-      Machine(flip(instructions, nopsAndJumps.head)).run match {
-        case (true, accum) => ChangeMachine(instructions, nopsAndJumps, accum)
-        case (false, _) => ChangeMachine(instructions, nopsAndJumps.tail, result)
-      }
-    }
-
-    @tailrec
-    final def solve: Int = if (hasNext) this.next.solve else result
-  }
-
-  case class MetaMachine(instructions: IndexedSeq[Instruction]) {
-    def run: Int = Machine(instructions).run match {
-      case (true, accum) => accum
-      case (false, _) =>
-        ChangeMachine(instructions, nopsAndJumps(instructions)).solve
-    }
-  }
-
-  case class Machine(instructions: IndexedSeq[Instruction]) {
-    def run: (Boolean, Int) = {
-      @tailrec
-      def recurse(cursor: Int = 0, accum: Int = 0, seen: Set[Int] = Set()): (Boolean, Int) = {
-        if (cursor == instructions.length) (true, accum)
-        else {
-          if (seen.contains(cursor)) (false, accum)
-          else {
-            instructions(cursor) match {
-              case Nop(_) => recurse(cursor + 1, accum, seen + cursor)
-              case Acc(n) => recurse(cursor + 1, accum + n, seen + cursor)
-              case Jmp(n) => recurse(cursor + n, accum, seen + cursor)
-            }
-          }
-        }
-      }
-
-      recurse()
-    }
-  }
+object Compiler extends RegexParsers {
 
   sealed trait Instruction
 
@@ -68,18 +13,72 @@ trait Day08 extends RegexParsers {
 
   case class Acc(x: Int) extends Instruction
 
-  class Compiler extends RegexParsers {
+  def nop: Parser[Nop] = "nop" ~> "[+-]?\\d+".r ^^ { x => Nop(x.toInt) }
 
-    def nop: Parser[Nop] = "nop" ~> "[+-]?\\d+".r ^^ { x => Nop(x.toInt) }
+  def jmp: Parser[Jmp] = "jmp" ~> "[+-]?\\d+".r ^^ { x => Jmp(x.toInt) }
 
-    def jmp: Parser[Jmp] = "jmp" ~> "[+-]?\\d+".r ^^ { x => Jmp(x.toInt) }
+  def acc: Parser[Acc] = "acc" ~> "[+-]?\\d+".r ^^ { x => Acc(x.toInt) }
 
-    def acc: Parser[Acc] = "acc" ~> "[+-]?\\d+".r ^^ { x => Acc(x.toInt) }
+  def allTokens: Parser[Instruction] = nop | jmp | acc
 
-    def compile: Parser[Instruction] = nop | jmp | acc
+  def compile(program: Iterable[String]): IndexedSeq[Instruction] =
+    program.map { line => parseAll(allTokens, line).get }.toIndexedSeq
+}
 
-    def compile(program: Iterable[String]): IndexedSeq[Instruction] =
-      program.map { line => parseAll(compile, line).get }.toIndexedSeq
+trait Day08 extends RegexParsers {
+
+  import Compiler._
+
+  case class MetaMachine(instructions: IndexedSeq[Instruction]) {
+    def nopsAndJumps(instructions: Seq[Instruction]): Seq[Int] =
+      instructions.zipWithIndex.filter(x => x._1.isInstanceOf[Nop] || x._1.isInstanceOf[Jmp]).map(_._2)
+
+    def run: Int = Machine(instructions).run match {
+      case (true, accum) => accum
+      case (false, _) => ChangeMachine(instructions, nopsAndJumps(instructions)).solve
+    }
+
+    private case class ChangeMachine(instructions: IndexedSeq[Instruction], nopsAndJumps: Seq[Int], result: Int = -1) {
+      def hasNext: Boolean = result == -1
+
+      def flip(instructions: IndexedSeq[Instruction], c: Int): IndexedSeq[Instruction] = {
+        instructions(c) match {
+          case Nop(x) => (instructions.take(c) :+ Jmp(x)) ++ instructions.drop(c + 1)
+          case Jmp(x) => (instructions.take(c) :+ Nop(x)) ++ instructions.drop(c + 1)
+          case _ => sys.error(s"")
+        }
+      }
+
+      def next: ChangeMachine = {
+        Machine(flip(instructions, nopsAndJumps.head)).run match {
+          case (true, accum) => ChangeMachine(instructions, nopsAndJumps, accum)
+          case (false, _) => ChangeMachine(instructions, nopsAndJumps.tail, result)
+        }
+      }
+
+      @tailrec
+      final def solve: Int = if (hasNext) this.next.solve else result
+    }
+
+  }
+
+  case class Machine(instructions: IndexedSeq[Instruction]) {
+    def run: (Boolean, Int) = {
+      @tailrec
+      def recurse(cursor: Int = 0, accum: Int = 0, seen: Set[Int] = Set()): (Boolean, Int) = {
+        if (cursor >= instructions.length) (true, accum)
+        else if (seen.contains(cursor)) (false, accum)
+        else {
+          instructions(cursor) match {
+            case Nop(_) => recurse(cursor + 1, accum, seen + cursor)
+            case Acc(n) => recurse(cursor + 1, accum + n, seen + cursor)
+            case Jmp(n) => recurse(cursor + n, accum, seen + cursor)
+          }
+        }
+      }
+
+      recurse()
+    }
   }
 
 }
--- src/test/scala/y2020/Day08Spec.scala
+++ src/test/scala/y2020/Day08Spec.scala
@@ -5,8 +5,7 @@ import org.scalatest.matchers.should._
 import util.Loader
 
 class Day08Spec extends AnyFlatSpec with Matchers with Day08 {
-  lazy val testInput =
-    """
+  lazy val testInput: IndexedSeq[Compiler.Instruction] = Compiler.compile( """
       |nop +0
       |acc +1
       |jmp +4
@@ -15,21 +14,24 @@ class Day08Spec extends AnyFlatSpec with Matchers with Day08 {
       |acc -99
       |acc +1
       |jmp -4
-      |acc +6""".stripMargin.trim.linesIterator.toIterable
+      |acc +6
+      |""".stripMargin.trim.linesIterator.toIterable)
+
+  lazy val input: IndexedSeq[Compiler.Instruction] = Compiler.compile(Loader(this, "day08.txt"))
 
   it should "pass the test case" in {
-    Machine(new Compiler().compile(testInput)).run shouldBe(false, 5)
+    Machine(testInput).run shouldBe(false, 5)
   }
 
   it should "solve the puzzle" in {
-    new Machine(new Compiler().compile(Loader(this, "day08.txt"))).run shouldBe(false, 1262)
+    Machine(input).run shouldBe(false, 1262)
   }
 
   it should "solve the second test case" in {
-    MetaMachine(new Compiler().compile(testInput)).run shouldBe 8
+    MetaMachine(testInput).run shouldBe 8
 
   }
   it should "solve the second puzzle" in {
-    new MetaMachine(new Compiler().compile(Loader(this, "day08.txt"))).run shouldBe 1643
+    MetaMachine(input).run shouldBe 1643
   }
 }
